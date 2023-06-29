06b761fe393005b08b176ebc4ab5cf6a63fb5b7c
Hemanth reddy <91110845+hemanth-reddy-git@users.noreply.github.com>
2023-06-29 14:42:09 +0530

    Add files via upload

--- screens/MyStack.js
+++ screens/MyStack.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-
-import { createStackNavigator } from '@react-navigation/stack';
 import Mainscreen from '../Home/Mainscreen';
-
+import { createStackNavigator } from '@react-navigation/stack';
+import Forgotpass from './Forgotpass';
+import Verifyotp from './Verifyotp';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}}/>
+      <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}/>
       <Stack.Screen name="Mainscreen" component={Mainscreen} />
- 
-
+      <Stack.Screen name="Forgotpass" component={Forgotpass} options={{headerShown:false}} />
+      <Stack.Screen name="Verifyotp" component={Verifyotp} />
     </Stack.Navigator>
   );
 }
